6ae45b26a2129a7f739ba614d6e7f4470bd3b523
wenjiewang98228 <33054589+wenjiewang98228@users.noreply.github.com>
2017-12-05 10:25:50 +0800

    Add files via upload

---
+++ Question1/Q1.java
@@ -0,0 +1,52 @@
+package Question_One;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("请输入1-99999之间的整数");
+        Scanner input = new Scanner(System.in);
+        int yourNumber = input.nextInt();
+        int YourNumber = yourNumber;
+       // System.out.println(yourNumber);
+        if(yourNumber >= 1 && yourNumber <= 99999){
+        	int a = 0;
+        //	System.out.println(a);
+        	while(yourNumber/10 != 0){
+        		a++;
+        //		System.out.println(a);
+        		yourNumber = yourNumber/10;
+        		
+        	}
+        //	System.out.println(a);
+        	int[] array = new int[a+1];
+        	for(int i=0;i<=a;i++)
+        	{
+        	    array[i] = YourNumber%10;
+        	    YourNumber = YourNumber/10;
+        //	    System.out.println(array[i]);
+        		}
+        	int h=0;
+        	int is_Number=1;
+        	while(h<=(a+1)/2)
+        	{
+        		if(array[h] == array[a-h]){
+        			h++;
+        		}
+        		else {
+        			is_Number=0;
+        			break;
+        		}
+        		}
+        	if(is_Number==1){
+        		System.out.println("yes");
+        	}
+        	else System.out.println("No");
+        		} 
+        else System.out.println("请输入1-99999之间的整数");
+        	}
+}
